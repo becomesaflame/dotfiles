2fd990ac102bb6ff1745e3045f3ca398ca10dd29
Steve Grandpre <Steven_D_Grandpre@raytheon.com>
2019-05-08 09:49:43 -0500

    Debugged setup.sh
    Still need to replace submodules with snapshots

--- setup.sh
+++ setup.sh
@@ -23,23 +23,24 @@ exit 0
 
 
 # Parse args
-while [[ $# -gt 0 ]] do
+while [[ $# -gt 0 ]]; do
   key="$1"
 
   case $key in
     -l|--locale)
-      locale = "$2"
-      if [ "$locale" -ne "work" ] || [ "$locale" -ne "home" ] then
-        echo 'Invalid locale.  Locale may be "home" or "work".'
+      locale="$2"
+      if [ "$locale" != "work" ] && [ "$locale" != "home" ]; then
+        echo "Invalid locale $locale.  Locale may be \"home\" or \"work\"."
         exit 1
       fi
       shift # past argument
       shift # past parameter
       ;;
     -p|--platform)
-      platform = "$2"
-      if [ "$platform" -ne "work" ] || [ "$platform" -ne "home" ] then
-        echo 'Invalid platform.  Platform may be "linux" or "windows".'
+      echo "args are $@"
+      platform="$2"
+      if [ "$platform" != "linux" ] && [ "$platform" != "windows" ]; then
+        echo "Invalid platform $platform.  Platform may be \"linux\" or \"windows\"."
         exit 1
       fi
       shift # past argument
@@ -53,36 +54,39 @@ done
 
 
 # Set up symbolic links in home directory
-ln -s .bashrc ~/.bashrc
-ln -s .vimrc ~/.vimrc
-ln -s .gitconfig ~/.gitconfig
-ln -s .tmux.conf ~/.tmux.conf
+rootDir=$(pwd)
+ln -s $rootDir/.bashrc ~/.bashrc
+ln -s $rootDir/.vimrc ~/.vimrc
+ln -s $rootDir/.gitconfig ~/.gitconfig
+ln -s $rootDir/.tmux.conf ~/.tmux.conf
 
 
 # Set up locale files
-if [ "$locale" -eq "work" ] then 
-  ln -s ".vimrc.local.work" ~/.vimrc.local
-  ln -s ".bashrc.local.work.$platform" ~/.bashrc.local
-  ln -s ".gitconfig.local.work.$platform" ~/.gitconfig.local
+if [ "$locale" == "work" ]; then 
+  ln -s "$rootDir/.vimrc.local.work" ~/.vimrc.local
+  ln -s "$rootDir/.bashrc.local.work.$platform" ~/.bashrc.local
+  ln -s "$rootDir/.gitconfig.local.work.$platform" ~/.gitconfig.local
 else
-  ln -s ".gitconfig.local.home" ~/.gitconfig.local
+  ln -s "$rootDir/.gitconfig.local.home" ~/.gitconfig.local
 fi
 
 
 # Set up vim plugged
-internet = curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+internet=$?
+echo "internet = $internet"
 
-if [ "$internet" -ne 0 ] then
-  cp -r /git/dotfiles/.tmux/ ~
+if [ "$internet" != 0 ]; then
+  cp -r $rootDir/.vim/ ~
 fi
 
 echo "Reload .vimrc and :PlugInstall to install vim plugins."
 
 
 # Set up tpm (tmux plugin manager)
-if [ "$internet" -ne 0 ] then
-  cp -r /git/dotfiles/.tmux/ ~
+if [ "$internet" != 0 ]; then
+  cp -r $rootDir/.tmux/ ~
 else
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
@@ -91,11 +95,11 @@ echo "In tmux, enter prefix + I to install plugins"
 
 
 # Set up tldr
-[ -d "~/bin" ] || mkdir ~/bin
+[ -d ~/bin ] || mkdir ~/bin
 cp raylee-tldr/tldr ~/bin/
 source ~/.bashrc # assuming that .bashrc adds ~/bin to PATH
 
-if [ "$internet" -ne 0 ] then
+if [ "$internet" != 0 ]; then
   cp .config ~
 else
   tldr -c
@@ -103,6 +107,6 @@ fi
 
 
 # Set up diff-so-fancy
-[ -d "~/bin" ] || mkdir ~/bin
+[ -d ~/bin ] || mkdir ~/bin
 cp diff-so-fancy/diff-so-fancy ~/bin/
 source ~/.bashrc # assuming that .bashrc adds ~/bin to PATH
